459e1e457dcaa3af568c34e437d76a11e024d32d
Amir Muhammad Hakim <amir.muh.hakim@gmail.com>
2021-05-22 07:52:08 +0700

    🚑 Fix assets: Using CDN

--- application/views/layouts/app.php
+++ application/views/layouts/app.php
@@ -4,20 +4,15 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta name="description" content="">
-	<meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-	<meta name="generator" content="Jekyll v3.8.6">
+	<meta name="description" content="Aplikasi toko online menggunakan CodeIgniter 3">
+	<meta name="author" content="Amir Muhammad Hakim">
+
 	<title><?= isset($title) ? $title : "CIShop" ?> - CodeIgniter E-Commerce</title>
 
 	<link rel="canonical" href="https://getbootstrap.com/docs/4.4/examples/navbar-fixed/">
-
-	<!-- Bootstrap core CSS -->
-	<link href="/assets/libs/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-	<!-- Fontawesome CSS -->
-	<link rel="stylesheet" href="/assets/libs/font-awesome/css/all.min.css">
-
-	<link rel="stylesheet" href="/assets/css/app.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<link rel="stylesheet" href="<?= base_url("/assets/css/app.css") ?>">
 </head>
 
 <body>
@@ -25,13 +20,14 @@
 	<?php $this->load->view('layouts/_navbar') ?>
 	<!-- End Navbar -->
 
-    <!-- Content -->
-    <?php $this->load->view($page) ?>
-    <!-- End Content -->
+	<!-- Content -->
+	<?php $this->load->view($page) ?>
+	<!-- End Content -->
 
-    <script src="/assets/libs/jquery/jquery-3.4.1.min.js"></script>
-    <script src="/assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="/assets/js/app.js"></script>
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	<script src="<?= base_url("/assets/js/app.js") ?>"></script>
 </body>
 
 </html>
